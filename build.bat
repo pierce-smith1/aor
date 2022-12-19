08821c6c011ffaed3eb8c44b0e2ef43d69bdc7a5
pierce-smith1 <psmith36@rams.colostate.edu>
2022-12-18 18:43:35 -0700

    #52

--- build.bat
+++ build.bat
@@ -6,18 +6,18 @@ for %%I in (%CD%\assets\img\items\*.png) do (
 
 echo ^<!DOCTYPE RCC^>^<RCC version="1.0"^>^<qresource^> > images.qrc
 for %%I in (%CD%\assets\img\items\*.png) do (
-    echo ^<file^>assets^/img^/items^/%%~nxI^</file^> >> images.qrc
+    echo ^<file^>assets/img/items/%%~nxI^</file^> >> images.qrc
 )
 for %%I in (%CD%\assets\img\items\sil\*.png) do (
-    echo ^<file^>assets^/img^/items^/sil^/%%~nxI^</file^> >> images.qrc
+    echo ^<file^>assets/img/items/sil/%%~nxI^</file^> >> images.qrc
 )
 for %%I in (%CD%\assets\img\icons\*.png) do (
-    echo ^<file^>assets^/img^/icons^/%%~nxI^</file^> >> images.qrc
+    echo ^<file^>assets/img/icons/%%~nxI^</file^> >> images.qrc
 )
 for %%I in (%CD%\assets\img\lk\*.png) do (
-    echo ^<file^>assets^/img^/icons^/%%I^</file^> >> images.qrc
+    echo ^<file^>assets/img/lk/%%~nxI^</file^> >> images.qrc
 )
-echo ^<\qresource^>^</RCC^> >> images.qrc
+echo ^</qresource^>^</RCC^> >> images.qrc
 
 qmake
 nmake release
