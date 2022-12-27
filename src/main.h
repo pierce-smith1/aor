3ccaf0863b8bb14410ea2707ef083650f28dc54c
pierce-smith1 <psmith36@rams.colostate.edu>
2022-12-27 02:49:11 -0700

    b1.1.0 rc

--- src/main.h
+++ src/main.h
@@ -14,7 +14,7 @@
 #include "cheatconsole.h"
 #include "encyclopedia.h"
 
-const static QString GAME_VERSION = "1.0.1";
+const static QString GAME_VERSION = "1.1.0";
 
 void new_game_prompt();
 void welcome_message();
